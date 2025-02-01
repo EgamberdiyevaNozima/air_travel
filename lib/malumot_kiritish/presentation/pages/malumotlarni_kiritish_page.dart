a6814aab5b4a15f477b31d6980f64d6c9a3ffbff
EgamberdiyevaNozima <sohibjonovna03@gmail.com>
2025-02-01 21:29:56 +0500

    image_picker

--- lib/malumot_kiritish/presentation/pages/malumotlarni_kiritish_page.dart
+++ lib/malumot_kiritish/presentation/pages/malumotlarni_kiritish_page.dart
@@ -4,41 +4,47 @@ import '../widgets/input_field_widget.dart';
 import '../widgets/dropdown_widget.dart';
 import '../widgets/save_button_widget.dart';
 
-class MalumotlarniKiritishPage extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Ma'lumotlarni kiriting"),
-      leading: BackButton(),
-    ),
-    body: Padding(
-      padding:  EdgeInsets.all(17.0),
-      child: Column(
-        children: [
-          ProfilePictureWidget(),
-          SizedBox(height: 20),
-          InputFieldWidget(labelText: "Ismingiz"),
-          SizedBox(height: 10),
-          InputFieldWidget(labelText: "Familiyangiz"),
-          SizedBox(height: 10),
-          InputFieldWidget(
-            labelText: "+998 77 777-07-77",
-            keyboardType: TextInputType.phone,
-          ),
-          SizedBox(height: 10),
-          DropdownWidget(
-            items: ["Toshkent", "Andijon", "Farg'ona", "Buxoro","Navoiy"],
-            hintText: "Viloyatingiz",
-          ),
-          SizedBox(height: 20),
-          SaveButtonWidget(
-            onPressed: () {
-            },
-          ),
-        ],
-      ),
-    ),
-  );
+class MalumotlarniKiritishPage extends StatefulWidget {
+  @override
+  State<MalumotlarniKiritishPage> createState() => _MalumotlarniKiritishPageState();
 }
+
+class _MalumotlarniKiritishPageState extends State<MalumotlarniKiritishPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ma'lumotlarni kiriting"),
+        leading: BackButton(),
+      ),
+      body: Padding(
+        padding:  EdgeInsets.all(17.0),
+        child: Column(
+          children: [
+            ProfilePictureWidget(),
+            SizedBox(height: 20),
+            InputFieldWidget(labelText: "Ismingiz"),
+            SizedBox(height: 10),
+            InputFieldWidget(labelText: "Familiyangiz"),
+            SizedBox(height: 10),
+            InputFieldWidget(
+              labelText: "+998 77 777-77-77",
+              keyboardType: TextInputType.phone,
+            ),
+            SizedBox(height: 10),
+            DropdownWidget(
+              items: ["Toshkent", 'Sirdaryo','Jizzax','Samarqand','Navoiy',"Buxoro",'Qashqadaryo','Surhandaryo',"Andijon", "Farg'ona",'Namangan' ],
+              hintText: "Viloyatingiz",
+            ),
+            SizedBox(height: 20),
+            SaveButtonWidget(
+              onPressed: () {
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
